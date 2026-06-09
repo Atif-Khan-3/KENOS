fa56414e5e398145b98e72bee81ab5c10a9d91bd
Muhammad Hussnain Saeed <m.hussnainsaeed@gmail.com>
2026-06-09 13:10:03 +0500

    Splash Screen has been added

--- MeasuringApp/App/RootView.swift
+++ MeasuringApp/App/RootView.swift
@@ -9,15 +9,27 @@ import Foundation
 import SwiftUI
 
 struct RootView: View {
-
-  
-   @StateObject var manager = UserPreferencesManager.shared
+    
+    @State private var showSplashScreen = true
+    @StateObject var manager = UserPreferencesManager.shared
 
     var body: some View {
-        if !manager.isProfileCreated {
-            ProfileCard(isupdate: false)
-        } else {
-            HomeView()
+        ZStack{
+            if !manager.isProfileCreated {
+                ProfileCard(isupdate: false)
+            } else {
+                HomeView()
+            }
+            if showSplashScreen {
+                VideoSplashView {
+                    // This closure runs when the video finishes
+                    withAnimation(.easeOut(duration: 1.0 )) {
+                        showSplashScreen = false
+                    }
+                }
+                //.transition(.opacity) // Fades out smoothly
+                .zIndex(1) // Ensures it stays on top of the app
+            }
         }
     }
 }
